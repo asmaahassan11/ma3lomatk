681a74b6f20b6227e988285ce9de54f38b48f614
asmaahassan11 <74038151+asmaahassan11@users.noreply.github.com>
2022-03-16 21:56:13 +0200

    asmaa

--- lib/components/rounded_email_field.dart
+++ lib/components/rounded_email_field.dart
@@ -28,8 +28,11 @@ class RoundedEmailField extends StatelessWidget {
           border: InputBorder.none,
         ),
         validator: (value) {
-          if (value.isEmpty) {
-            return 'Required !';
+          if (value == null || value.isEmpty) {
+            return 'This field is required';
+          }
+          if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+            return "Please enter a valid email address";
           }
           return null;
         },
